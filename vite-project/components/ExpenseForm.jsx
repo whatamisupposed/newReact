b3d958ae3e1a44f0c0804182f18eee0b4c3c7c1f
Emily Twyman <twymanem@gmail.com>
2024-04-17 11:40:18 -0600

    lessee here

--- vite-project/components/ExpenseForm.jsx
+++ vite-project/components/ExpenseForm.jsx
@@ -1,46 +1,41 @@
-import React from 'react'
-import { useForm } from "react-hook-form"
+import React from 'react';
+import { useForm } from "react-hook-form";
 
-const ExpenseForm = ( {addExpense}) => {
-
-    const { register, handleSubmit, reset, formState: { errors} } = useForm()
+const ExpenseForm = ({ addExpense }) => {
+  const { register, handleSubmit, reset, formState: { error } } = useForm();
 
   return (
-    <form action="" className="mb-3" onSubmit = {handleSubmit( (data) => {
-        addExpense;
-        reset(data)
-  })}>
+    <form action="" className="mb-3" onSubmit={handleSubmit((data) => {
+      addExpense(data);
+      reset(data);
+    })}>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">Description </label>
+        <input id="description" type="text" className="form-control" {...register("description", { minLength: 6, required: true })} />
+        {error.description?.type === "required" && <p className="text-danger">The field is required</p>}
+        {error.description?.type === "minLength" && <p className="text-danger">This field requires at least 6 characters</p>}
+      </div>
 
-<div className="mb-3">
-    <label htmlFor="description" className="form-label">Description </label>
-    <input id="description" type="text" className="form-control" { ...register("description"), {minLength:6, required:true}}/>
-    {error.description?.type == "required" && <p className="text-danger">The field is required</p>}
-    {error.description?.type == "minLength" &&  <p className="text-danger">This field requires at least 6 characters</p>}
-</div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">Amount </label>
+        <input id="amount" type="text" className="form-control" {...register("amount", { required: true })} />
+        {error.amount?.type === "required" && <p className="text-danger">The field is required</p>}
+      </div>
 
-<div className="mb-3">
-    <label htmlFor="amount" className="form-label">Amount </label>
-    <input id="amount" type="text" className="form-control" { ...register("amount",{required:true})}/>
-    {error.amount?.type == "required" && <p className="text-danger">The field is required</p>}
-</div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">Category </label>
+        <select id="category" className="form-select" {...register("category", { required: true })}>
+          <option value=""></option>
+          <option value="utilities">Utilities</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="groceries">Groceries</option>
+        </select>
+        {error.category?.type === "required" && <p className="text-danger">The field is required</p>}
+      </div>
 
-<div className="mb-3">
-    <label htmlFor="description" className="form-label">Category </label>
-
-    <select name="" id="" className="form-select" { ...register("category"), {required:true}}>
-       <option value=""></option>
-        <option value="utilities">Utilities</option>
-        <option value="entertainment">Entertainment</option>
-        <option value="groceries">Groceries</option>
-    </select>
-
-    {error.category?.type == "required" && <p className="text-danger">The field is required</p>}
-</div>
-
-<button className="btn btn-primary">Submit</button>
-
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
-  )
+  );
 }
 
-export default ExpenseForm
+export default ExpenseForm;
--- vite-project/components/ExpenseList.jsx
+++ vite-project/components/ExpenseList.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
 
-const ExpenseList = (items, deleteItem) => {
+
+import React from 'react';
+
+const ExpenseList = ({ items, deleteItem }) => {
   return (
-   <table className="table">
-    <thead>
+    <table className="table">
+      <thead>
         <tr>
-            <th scope="col">Description</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Category</th>
+          <th scope="col">Description</th>
+          <th scope="col">Amount</th>
+          <th scope="col">Category</th>
         </tr>
-    </thead>
-    <tbody>
-
-{ items.map((item, index) =>  <tr key = {index}>
-<td>{items.description}</td>
-<td>{item.amount}</td>
-<td>{item.category}</td>
-<td><button className="btn btn-outline-danger" onClick={() =>deleteItem(item.id)}>Delete</button></td>
-</tr>
-)}
-<tr>
-    <td><h3>Total</h3></td>
-    <td><h3>${ items.reduce((total, item) => total + parseInt(item.amount),0).toFixed(2)}</h3></td>
-</tr>
-    </tbody>
-   </table>
-  )
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.description}</td>
+            <td>{item.amount}</td>
+            <td>{item.category}</td>
+            <td><button className="btn btn-outline-danger" onClick={() => deleteItem(item.id)}>Delete</button></td>
+          </tr>
+        ))}
+        <tr>
+          <td><h3>Total</h3></td>
+          <td><h3>${items.reduce((total, item) => total + parseInt(item.amount), 0).toFixed(2)}</h3></td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
-export default ExpenseList
+export default ExpenseList;
