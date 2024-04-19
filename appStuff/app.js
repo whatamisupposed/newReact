a732acd26af1b781548a3fb7f87ab975f5e53d1d
Emily Twyman <twymanem@gmail.com>
2024-04-19 07:29:22 -0600

    fff

--- appStuff/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TransactionList from './TransactionList';
-import TransactionForm from './TransactionForm';
-import ExpenseTrackerProvider from './ExpenseTrackerProvider';
-const App = () => {
-  return /*#__PURE__*/React.createElement(ExpenseTrackerProvider, null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Expense Tracker"), /*#__PURE__*/React.createElement(TransactionList, null), /*#__PURE__*/React.createElement(TransactionForm, null)));
-};
-export default App;
-
--- vite-project/components/ExpenseForm.jsx
+++ vite-project/components/ExpenseForm.jsx
@@ -1,41 +1,81 @@
-import React from 'react';
 import { useForm } from "react-hook-form";
 
 const ExpenseForm = ({ addExpense }) => {
-  const { register, handleSubmit, reset, formState: { error } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { error },
+  } = useForm();
 
   return (
-    <form action="" className="mb-3" onSubmit={handleSubmit((data) => {
-      addExpense(data);
-      reset(data);
-    })}>
+    <form
+      action=""
+      className="mb-3"
+      onSubmit={handleSubmit((data) => {
+        addExpense(data);
+        reset(data);
+      })}
+    >
       <div className="mb-3">
-        <label htmlFor="description" className="form-label">Description </label>
-        <input id="description" type="text" className="form-control" {...register("description", { minLength: 6, required: true })} />
-        {error.description?.type === "required" && <p className="text-danger">The field is required</p>}
-        {error.description?.type === "minLength" && <p className="text-danger">This field requires at least 6 characters</p>}
+        <label htmlFor="description" className="form-label">
+          Description{" "}
+        </label>
+        <input
+          id="description"
+          type="text"
+          className="form-control"
+          {...register("description", { minLength: 6, required: true })}
+        />
+        {error?.description?.type === "required" && (
+          <p className="text-danger">The field is required</p>
+        )}
+        {error?.description?.type === "minLength" && (
+          <p className="text-danger">
+            This field requires at least 6 characters
+          </p>
+        )}
       </div>
 
       <div className="mb-3">
-        <label htmlFor="amount" className="form-label">Amount </label>
-        <input id="amount" type="text" className="form-control" {...register("amount", { required: true })} />
-        {error.amount?.type === "required" && <p className="text-danger">The field is required</p>}
+        <label htmlFor="amount" className="form-label">
+          Amount{" "}
+        </label>
+        <input
+          id="amount"
+          type="text"
+          className="form-control"
+          {...register("amount", { required: true })}
+        />
+        {error?.amount?.type === "required" && (
+          <p className="text-danger">The field is required</p>
+        )}
       </div>
 
       <div className="mb-3">
-        <label htmlFor="category" className="form-label">Category </label>
-        <select id="category" className="form-select" {...register("category", { required: true })}>
+        <label htmlFor="category" className="form-label">
+          Category{" "}
+        </label>
+        <select
+          id="category"
+          className="form-select"
+          {...register("category", { required: true })}
+        >
           <option value=""></option>
           <option value="utilities">Utilities</option>
           <option value="entertainment">Entertainment</option>
           <option value="groceries">Groceries</option>
         </select>
-        {error.category?.type === "required" && <p className="text-danger">The field is required</p>}
+        {error?.category?.type === "required" && (
+          <p className="text-danger">The field is required</p>
+        )}
       </div>
 
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
-}
+};
 
 export default ExpenseForm;
